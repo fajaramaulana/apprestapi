588952316752a875e867cc657be78f5473eafd10
fajaramaulana <59821005+fajaramaulana@users.noreply.github.com>
2021-02-26 21:40:21 +0700

    success show all mahasiswa data

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require("./res");
 var connection = require("./connection");
 
 exports.index = function (req, res) {
-  response.ok("REST API RUNNING !");
+  response.ok("REST API RUNNING !", res);
+};
+
+// show all data mahasiswa
+exports.showAllMahasiswa = function (req, res) {
+  connection.query("SELECT * FROM mahasiswa", function (err, rows, fields) {
+    if (err) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
 };
--- routes.js
+++ routes.js
@@ -3,4 +3,5 @@ module.exports = function (app) {
   var myJson = require("./controller");
 
   app.route("/").get(myJson.index);
+  app.route("/show").get(myJson.showAllMahasiswa);
 };
