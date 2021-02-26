640e90e55f38b0c69b560db25398c21412b31d66
fajaramaulana <59821005+fajaramaulana@users.noreply.github.com>
2021-02-26 21:45:14 +0700

    success show data by id

--- controller.js
+++ controller.js
@@ -11,9 +11,26 @@ exports.index = function (req, res) {
 exports.showAllMahasiswa = function (req, res) {
   connection.query("SELECT * FROM mahasiswa", function (err, rows, fields) {
     if (err) {
-      connection.log(error);
+      console.log(err);
     } else {
       response.ok(rows, res);
     }
   });
+};
+
+// show all mahasiswa data by id
+
+exports.showMahasiswaById = function (req, res) {
+  let id = req.params.id;
+  connection.query(
+    "SELECT * FROM mahasiswa WHERE id = ?",
+    [id],
+    function (err, rows, fields) {
+      if (err) {
+        console.log(err);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
 };
