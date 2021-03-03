22467b02a371a38b3d6ba6456961d0963c589da5
fajaramaulana <59821005+fajaramaulana@users.noreply.github.com>
2021-03-03 20:18:49 +0700

    change port to 3001, install nodemon and cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 // parse application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 // call routes
 let routes = require("./routes");
@@ -15,6 +17,6 @@ routes(app);
 // register menu routes from middleware/index.js
 app.use("/auth", require("./middleware"));
 
-app.listen(3000, () => {
-  console.log(`Server started on port 3000`);
+app.listen(3001, () => {
+  console.log(`Server started on port 3001`);
 });
